96387cfe3dd75297e58fa506391bdb87235f75d0
dmwilcox <dmwilcox@gmail.com>
2016-01-22 00:14:07 -0800

    Now returns all encoded URLs contained requested URL or fragment.

--- cleanurl.py
+++ cleanurl.py
@@ -2,50 +2,75 @@
 
 """Extract an encoded URL from the parameters of a wrapping URL.
 
-Pulls out an encoded URL such as a link embedded in tracking URL from
-Facebook or Google.
+Pulls out an encoded URLs from tracking links. Share the link with
+your friend not the associated tracking cookies.
+
 """
 
+import re
 import urlparse
 
 
-def is_url(maybe_url):
-    """Check to see if a provided string is a full URL.
+# Thanks Uncle Berners-Lee (https://tools.ietf.org/html/rfc3986#appendix-B)
+URL_REGEX = (r'(([^:/?#]+):)?'   # protocol, and protocol with ':'
+              '(//([^/?#]*))?'   # //domain and domain
+              '([^?#]*)'         # path
+              '(\?([^#]*))?'     # query with and without leading '?'
+              '(#(.*))?')        # anchor with and without leading '#'
+URL_REG = re.compile(URL_REGEX)
 
-    Does not work with URLs w/o a protocol since urlparse module,
-    can not not determine the hostname, which is fixable by doing
-    a DNS query, if desired. TBD.
+
+# Without enforcing some order ("http[s]?://something" is the minimum)
+# the regex messes up; there are many match targets available.
+# Extra punctuation is disallowed to handle things like encoded JSON in
+# the URL.
+ORDER_REGEX = (r'([^:/#,"\'{}]+)://'  # matches protocol and ://
+                '([^/?#,"\'{}]*)'     # domain
+                '([^?#,"\'{}]*)'      # path up until '#' or '?'
+                '(\?([^?,"\'{}]*))?'  # optionally a query string
+                '(#([^\W]*))?')       # optionally an anchor
+ORDER_REG = re.compile(ORDER_REGEX)
+
+
+def url_encoding_table():
+    """Return a dict mapping ascii characters to URL encoded characters.
+
+    Why there isn't a forward 'quote' function in urlparse I don't know.
     """
-    if isinstance(maybe_url, list):
-        assert len(maybe_url) == 1
-        maybe_url = maybe_url[0]
-
-    url_pieces = urlparse.urlparse(maybe_url)
-    if not all([url_pieces.scheme, url_pieces.netloc]):
-        return False
-    else:
-        return True
+    return dict([ (urlparse.unquote('%%%02x' % i), '%%%02x' % i)
+                  for i in range(1 << 7) ])
 
 
-def extract_url(url):
-    """Pull out a URL from the encoded parameters of another URL.
+def get_url(s):
+    """Return an iterator of regex matches or None."""
+    #match = URL_REG.search(s)
+    #match = ORDER_REG.search(s)
+    #if not match:
+    #    return
+    return ORDER_REG.finditer(s)
 
-    Assumes there is only 1 URL encoded in the parameters, the first
-    by hash order is returned.  Also fixable by changing to a generator
-    function and calling it more than once, needed? TBD.
+
+def get_urls(url):
+    """Take the given URL or URL fragment and return any encoded URLs.
+
+    Call get_url for every parameter value and yield any results
+    they return.
     """
+    # even if nothing is recognized the whole string is returned in 'query'
+    # which is how query fragments are supported
     query_str = urlparse.urlparse(url).query
     if query_str:
         query_data = urlparse.parse_qs(query_str)
         for key in query_data:
-            if is_url(query_data[key]):
-                # ugh, urlparse always seems to return lists, ugly
-                return query_data[key][0]
+            for url_match in ORDER_REG.finditer(query_data[key][0]):
+                yield url_match.group()
 
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) > 2:
-        print 'Usage: %s "http://trackingurl.com?u=http%3A%2F%2Fwww.wanttogetto.com..."' % sys.argv[0]
+        print "Usage: %s 'https://goog-fb-adtrack.com/?url=http%3A%2F%2Fwww.youtube.com...'" % sys.argv[0]
         sys.exit(1)
-    print extract_url(sys.argv[1])
+
+    for url in get_urls(sys.argv[1]):
+        print url
